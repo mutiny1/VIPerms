6684822de97736ab902198496c49dbfcfdc6c0b5
mutiny1 <mutiny1@gmail.com>
2024-09-06 12:14:51 -0500

    Update Set-CertPolicy.ps1 for PWSH 7

--- VIPerms/Private/Set-CertPolicy.ps1
+++ VIPerms/Private/Set-CertPolicy.ps1
@@ -2,10 +2,9 @@ function Set-CertPolicy {
     <#
     .SYNOPSIS
     Ignore SSL verification.
-    
+
     .DESCRIPTION
     Using a custom .NET type, override SSL verification policies.
-
     #>
 
     param (
@@ -15,30 +14,34 @@ function Set-CertPolicy {
 
     try {
         if ($SkipCertificateCheck) {
-            try {
-                [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls"
-                [Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-            } catch {
-                $Err = $_
-                if ($Err.Exception.Message.StartsWith("Cannot find type [TrustAllCertsPolicy]")) {
-                    Add-Type -TypeDefinition  @"
-                    using System.Net;
-                    using System.Security.Cryptography.X509Certificates;
-                    public class TrustAllCertsPolicy : ICertificatePolicy {
-                        public bool CheckValidationResult(
-                            ServicePoint srvPoint, X509Certificate certificate,
-                            WebRequest request, int certificateProblem) {
-                            return true;
-                        }
-                    }
-"@
-                    [Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-                } else {
-                    throw $Err
+            if ($PSVersionTable.PSEdition -eq 'Core') {
+                # Invoke-restmethod provide Skip certcheck param in PowerShell Core
+                $Script:PSDefaultParameterValues = @{
+                    "invoke-restmethod:SkipCertificateCheck" = $true
+                    "invoke-webrequest:SkipCertificateCheck" = $true
                 }
+            } else {
+                # Load the assembly containing TrustAllCertsPolicy if not already loaded
+                $assemblyName = 'System.Net.Http'
+                $loadedAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object { $_.GetName().Name }
+
+                if (-not $loadedAssemblies.Contains($assemblyName)) {
+                    Add-Type -AssemblyName $assemblyName
+                }
+
+                Add-Type -TypeDefinition  @"
+                using System.Net;
+                using System.Security.Cryptography.X509Certificates;
+                public class TrustAllCertsPolicy : ICertificatePolicy {
+                    public bool CheckValidationResult(
+                        ServicePoint srvPoint, X509Certificate certificate,
+                        WebRequest request, int certificateProblem) {
+                        return true;
+                    }
+                }
+"@
+                [Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
             }
-        } else {
-            [Net.ServicePointManager]::CertificatePolicy = $null
         }
     } catch {
         $Err = $_
